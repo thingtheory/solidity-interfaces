84cf8d795674af3cd5e26d11a796a2f5dfa661f0
thingtheory <thingtheory@protonmail.com>
2022-03-06 11:42:34 -0800

    update beetsbar interface

--- src/beethovenx/IBeetsBar.sol
+++ src/beethovenx/IBeetsBar.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-interface IBeetsBar {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IBeetsBar is IERC20 {
   function enter(uint256 amount_) external;
   function leave(uint256 shareOfFreshBeets_) external;
 }
